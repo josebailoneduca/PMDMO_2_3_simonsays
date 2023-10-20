8e47ab6737a95655de9ee5691ad71eb6e274d1da
Jose Bailon <josebailoneduca@gmail.com>
2023-10-20 18:29:48 +0200

    constructor de botonera a medias

--- app/src/main/java/com/imagenprogramada/simonsays/EDificultad.kt
+++ app/src/main/java/com/imagenprogramada/simonsays/EDificultad.kt
@@ -1,7 +1,8 @@
 package com.imagenprogramada.simonsays
 
-enum class EDificultad(filas: Int, columnas: Int, nombre: Int) {
+enum class EDificultad(val filas: Int, val columnas: Int, val nombre: Int) {
     EASY(2,2,R.string.easy),
     MEDIUM(4, 2, R.string.medium),
-    HARD(6,2, R.string.hard)
+    HARD(6,2, R.string.hard);
+
 }
--- app/src/main/java/com/imagenprogramada/simonsays/InicioActivity.kt
+++ app/src/main/java/com/imagenprogramada/simonsays/InicioActivity.kt
@@ -1,9 +1,13 @@
 package com.imagenprogramada.simonsays
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.gridlayout.widget.GridLayout
 
 class InicioActivity : AppCompatActivity() {
     lateinit var juego:Juego
@@ -23,7 +27,6 @@ class InicioActivity : AppCompatActivity() {
                 R.id.rbDificultyHard -> juego.dificultad=EDificultad.HARD;
             }
         }
-
         findViewById<Button>(R.id.btnPlay).setOnClickListener({ v ->
             iniciarJuego()
         })
@@ -31,5 +34,51 @@ class InicioActivity : AppCompatActivity() {
 
     private fun iniciarJuego() {
         setContentView(R.layout.partida)
+
+        val difi:EDificultad =juego.dificultad
+        val contenedor = findViewById<ConstraintLayout>(R.id.lyContenedor)
+        //limpiar contenedor
+        contenedor.removeAllViews()
+
+        //agregar grid
+        val grid:GridLayout=crearGrid(difi.filas,difi.columnas)
+        contenedor.addView(grid)
+        val gParams = grid.layoutParams
+        gParams.width=GridLayout.LayoutParams.MATCH_PARENT
+        gParams.height=GridLayout.LayoutParams.MATCH_PARENT
+        grid.layoutParams=gParams
+
+        //generar botones
+        var i=0
+        for (c in 0..< difi.columnas){
+            for (f in 0..< difi.filas){
+                val btn:Button = crearBoton(""+i,c,f)
+                val params:GridLayout.LayoutParams=GridLayout.LayoutParams(GridLayout.spec(f, 1,1f), GridLayout.spec(c, 1,1f))
+                params.height=0
+                params.width=0
+                btn.layoutParams=params
+                btn.layoutParams=params;
+
+
+
+                grid.addView(btn,params)
+            }
+        }
+    }
+
+    private fun crearGrid(filas: Int, columnas: Int): GridLayout {
+        Log.i("jjbo","entrada crear grid")
+        var grid:GridLayout= GridLayout(this)
+        grid.columnCount=columnas
+        grid.rowCount=filas
+        grid.id=View.generateViewId()
+        return grid
+    }
+
+    private fun crearBoton( etiqueta:String,columna:Int,fila:Int):Button{
+        val btn:Button = Button(this)
+        btn.id= View.generateViewId()
+        return btn
+
     }
 }
